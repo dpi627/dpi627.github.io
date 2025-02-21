d7ed2e6458fab072e19f4467fecec9c208252256
brian_li <brian.li@sgs.com>
2025-02-21 16:20:59 +0800

    refactor: restructure index.html and move styles to site.css; add site.js for SweetAlert2 functionality

---
+++ assets/js/site.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    Swal.fire({
+        icon: 'info', // 警告圖示
+        title: '<div class="mt-4 mb-4">🤓 Hi there!<br> I\'m <span class="text-danger">Brian</span>, a software developer.</div>',
+        timer: 3500, // 5秒倒數計時
+        timerProgressBar: true, // 顯示進度條
+        showConfirmButton: true, // 顯示 OK 按鈕
+        confirmButtonText: 'OK', // 按鈕文字
+        allowOutsideClick: false, // 禁止點擊外部關閉
+        confirmButtonColor: '#4A8079', // 按鈕顏色
+    });
+};
